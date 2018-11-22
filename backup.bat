b8cc47133716e0c52da89b210826ed8f995d9f24
hunk.han <hunk.han@chinasystems.com>
2018-11-22 15:19:58 +0800

    daily Backup

---
+++ backup.bat
@@ -0,0 +1,15 @@
+@echo off
+cd %1
+git add .
+git commit -m "daily Backup"
+git config --global push.default simple
+git push 
+for /F %%i in ('git branch') do (
+	if %%i equ "*" ()
+	else (
+		git add .
+		git commit -m "daily Backup"
+		git config --global push.default simple
+		git push origin %%i:%%i
+	)
+)
